b2968bb11992e97287a5b7c7ae3ce6923a247f69
rmilby13 <rmilby+git@gmail.com>
2024-08-24 13:51:14 -0400

    Added debug messages.

--- ArduinoLibs/libraries/config/src/config.cpp
+++ ArduinoLibs/libraries/config/src/config.cpp
@@ -6,32 +6,47 @@
  */
 
 #include "config.h"
-
+//#define DEBUGCONFIG 1
+#ifdef TRACECONFIG
+#define DEBUGCONFIG 1
+#define TRACE(...) Serial.printf("\n%s:%d:\t",__FILE__,__LINE__); Serial.printf(__VA_ARGS__) ; Serial.println()
+#else
+#define TRACE(...)
+#endif
+#ifdef DEBUGCONFIG
+#define DEBUG(...) Serial.printf("\n%s:%d:\t",__FILE__,__LINE__); Serial.printf(__VA_ARGS__) ; Serial.println()
+#else
+#define DEBUG(...)
+#endif
 ConfigInfo::ConfigInfo () {
-  Serial.printf("%s:%d\n",__FILE__,__LINE__);
+  TRACE("Creating configuration");
 }
 
 ConfigInfo::~ConfigInfo() {
-  Serial.printf("%s:%d\n",__FILE__,__LINE__);
+  TRACE("Destroying configuration");
 }
 
 arduino::String ConfigInfo::display () {
+  TRACE("Starting ConfigInfo::display\n");
   arduino::String ret;
   serializeJsonPretty (this->createJsonDoc (), ret);
+  TRACE("Finished ConfigInfo::display\n");
   return ret;
 }
 
 jsondoc ConfigInfo::createJsonDoc () {
+  TRACE("Starting ConfigInfo::createJsonDoc");
   jsondoc doc;
-  //Serial.printf("%s:%d\n",__FILE__,__LINE__);
   return doc;
 }
 
 void ConfigInfo::save (File filehandle) {
+  TRACE("Starting ConfigInfo::save");
   serializeJson (this->createJsonDoc (), filehandle);
 }
 
 jsondoc ConfigInfo::load (File filehandle) {
+  TRACE("Starting ConfigInfo::load");
   jsondoc doc;
   deserializeJson (doc, filehandle);
   return doc;
