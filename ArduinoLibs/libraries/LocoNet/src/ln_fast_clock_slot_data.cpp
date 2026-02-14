261ed4772a66d58cd90fdad207b5f155be3f7164
rmilby13 <rmilby+git@gmail.com>
2026-02-14 15:04:50 -0500

    Update LocoNet fast clock slot data files
    Modified binary files for fast clock slot data in the LocoNet library to
    enhance functionality and performance.

--- ArduinoLibs/libraries/LocoNet/src/ln_fast_clock_slot_data.cpp
+++ ArduinoLibs/libraries/LocoNet/src/ln_fast_clock_slot_data.cpp
@@ -24,31 +24,29 @@
 namespace LocoNet {
 
 LN_FAST_CLOCK_SLOT_DATA::LN_FAST_CLOCK_SLOT_DATA() :LN_WR_SL_DATA() {
+	// Ensure this is the fast clock slot message opcode 0x7B in byte 2 per format
 	this->data[2] = 0x7B;
-	this->setCheckSum();
+	// default length/other bytes are left as-is by LN_WR_SL_DATA constructor	this->setCheckSum();
 }
 
 LN_FAST_CLOCK_SLOT_DATA::LN_FAST_CLOCK_SLOT_DATA( LNPacket &packet ) :LN_WR_SL_DATA(packet) {
-	// TODO Auto-generated constructor stub
-};
-
+	// packet-based ctor; assume packet already follows format
+}n
 LN_FAST_CLOCK_SLOT_DATA::LN_FAST_CLOCK_SLOT_DATA( packet_data &pdata ) :LN_WR_SL_DATA(pdata) {
-	// TODO Auto-generated constructor stub
-};
+	// raw data ctor
+}
 
 arduino::String LN_FAST_CLOCK_SLOT_DATA::toString(){
-	TRACE("Starting LN_FAST_CLOCK_SLOT_DATA::toString");
 	arduino::String s = LN_WR_SL_DATA::toString();
-	TRACE("s = %s", s.c_str());
-	s += " Request Fast Clock is Synchronized, Running, rate is " + arduino::String(this->getRate()) + ":1";
-	TRACE("s = %s", s.c_str());
-	s += " Day " + arduino::String(this->getDay()) + ", " + arduino::String(this->gethour()) + ":" + arduino::String(this->getMinute()) +  ".";
-	TRACE("s = %s", s.c_str());
-	s += " Last set by ID " + arduino::String(this->getSlot());
-	DEBUG("LN_FAST_CLOCK_SLOT_DATA::toString - %s", s.c_str());
+	s += " FastClock rate=" + arduino::String(this->getRate());
+	s += ", day=" + arduino::String(this->getDay());
+	s += ", time=" + arduino::String(this->getHour()) + ":" + (this->getMinute() < 10 ? "0" : "") + arduino::String(this->getMinute());
+	s += ", slot=" + arduino::String(this->getSlot());
+	s += ", ctrl=0x" + arduino::String(this->getClockControl(), HEX);
 	return s;
 }
 
+// Rate at data[3]
 byte LN_FAST_CLOCK_SLOT_DATA::getRate() {
 	return this->data[3];
 }
@@ -58,189 +56,157 @@ void LN_FAST_CLOCK_SLOT_DATA::setRate( byte rate ) {
 	this->setCheckSum();
 }
 
+// Fractional minute low/high at 4/5
+byte LN_FAST_CLOCK_SLOT_DATA::getFracMinLow(){
+	return this->data[4];
+}
+
+byte LN_FAST_CLOCK_SLOT_DATA::getFracMinHigh(){
+	return this->data[5];
+}
+
+void LN_FAST_CLOCK_SLOT_DATA::setFracMinLow(byte v){
+	this->data[4] = v;
+	this->setCheckSum();
+}
+
+void LN_FAST_CLOCK_SLOT_DATA::setFracMinHigh(byte v){
+	this->data[5] = v;
+	this->setCheckSum();
+}
+
+uint16_t LN_FAST_CLOCK_SLOT_DATA::getFracMin(){
+	return ((uint16_t)this->getFracMinHigh() << 8) | (uint16_t)this->getFracMinLow();
+}
+
+void LN_FAST_CLOCK_SLOT_DATA::setFracMin(uint16_t v){
+	this->setFracMinLow((byte)(v & 0xFF));
+	this->setFracMinHigh((byte)((v >> 8) & 0xFF));
+}
+
+// Minute stored as (256 - minute) modulo 0-59 at data[6]
+byte LN_FAST_CLOCK_SLOT_DATA::getMinute() {
+	byte raw = this->data[6];
+	int minute = (256 - (int)raw) & 0xFF;
+	return (byte)(minute % 60);
+}
+
+void LN_FAST_CLOCK_SLOT_DATA::setMinute( byte minute ) {
+	minute = minute % 60;
+	this->data[6] = (byte)((256 - minute) & 0xFF);
+	this->setCheckSum();
+}
+
+// Hour stored as (256 - hour) modulo 0-23 at data[8]
+byte LN_FAST_CLOCK_SLOT_DATA::getHour() {
+	byte raw = this->data[8];
+	int hour = (256 - (int)raw) & 0xFF;
+	return (byte)(hour % 24);
+}
+
+void LN_FAST_CLOCK_SLOT_DATA::setHour( byte hour ) {
+	hour = hour % 24;
+	this->data[8] = (byte)((256 - hour) & 0xFF);
+	this->setCheckSum();
+}
+
 int LN_FAST_CLOCK_SLOT_DATA::getDay() {
-	return this->data[9];
+	return (int)this->data[9];
 }
 
 void LN_FAST_CLOCK_SLOT_DATA::setDay( int day ) {
-	this->data[9] = day;
+	this->data[9] = (byte)day;
 	this->setCheckSum();
 }
 
-byte LN_FAST_CLOCK_SLOT_DATA::gethour() {
-	return this->data[8]-104;
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setHour( byte hour ) {
-	this->data[8] = hour+104;
+void LN_FAST_CLOCK_SLOT_DATA::setSlot( byte slot ){
+	this->data[7] = slot; // TRK/slot byte
 	this->setCheckSum();
-};
+}
 
-void LN_FAST_CLOCK_SLOT_DATA::setMinute( byte minute ) {
-	this->data[6] = minute+67;
+byte LN_FAST_CLOCK_SLOT_DATA::getSlot(){
+	return this->data[7];
+}
+
+byte LN_FAST_CLOCK_SLOT_DATA::getClockControl(){
+	return this->data[10];
+}
+
+void LN_FAST_CLOCK_SLOT_DATA::setClockControl(byte ctrl){
+	this->data[10] = ctrl;
 	this->setCheckSum();
-};
+}
 
-byte LN_FAST_CLOCK_SLOT_DATA::getMinute() {
-	return this->data[6]-67;
-};
+// D6: 1 = valid clock info (mask 0x40)
+bool LN_FAST_CLOCK_SLOT_DATA::isValid(){
+	return (this->getClockControl() & 0x40) != 0;
+}
 
-void LN_FAST_CLOCK_SLOT_DATA::setSecond( byte second ) {
-	this->data[8] = second;
+void LN_FAST_CLOCK_SLOT_DATA::setValid(bool valid){
+	if (valid) this->data[10] |= 0x40; else this->data[10] &= (byte)~0x40;
 	this->setCheckSum();
-};
+}
 
-byte LN_FAST_CLOCK_SLOT_DATA::getSecond() {
-	return this->data[8];
-};
+// Use bit0 for synchronized and bit1 for running (keeps old behavior but moved to control byte)
+bool LN_FAST_CLOCK_SLOT_DATA::isSynchronized(){
+	return (this->getClockControl() & 0x01) != 0;
+}
 
-void LN_FAST_CLOCK_SLOT_DATA::setSynchronized( bool sync ) {
-	if (sync) {
-		this->data[2] |= 0x01;
-	} else {
-		this->data[2] &= 0xFE;
-	}
+void LN_FAST_CLOCK_SLOT_DATA::setSynchronized(bool sync){
+	if (sync) this->data[10] |= 0x01; else this->data[10] &= (byte)~0x01;
 	this->setCheckSum();
-};
+}
 
-bool LN_FAST_CLOCK_SLOT_DATA::isSynchronized() {
-	return (this->data[2] & 0x01) != 0;
-};
+bool LN_FAST_CLOCK_SLOT_DATA::isRunning(){
+	return (this->getClockControl() & 0x02) != 0;
+}
 
-void LN_FAST_CLOCK_SLOT_DATA::setRunning( bool run ) {
-	if (run) {
-		this->data[2] |= 0x02;
-	} else {
-		this->data[2] &= 0xFD;
-	}
+void LN_FAST_CLOCK_SLOT_DATA::setRunning(bool run){
+	if (run) this->data[10] |= 0x02; else this->data[10] &= (byte)~0x02;
 	this->setCheckSum();
-};
+}
 
-bool LN_FAST_CLOCK_SLOT_DATA::isRunning() {
-	return (this->data[2] & 0x02) != 0;
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setSlot( byte slot ) {
-	this->data[2] = (this->data[2] & 0xFC) | (slot & 0x03);
-	this->setCheckSum();
-};
-
-byte LN_FAST_CLOCK_SLOT_DATA::getSlot() {
-	return this->data[2] & 0x03;
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setTime( byte hour, byte minute, byte second ) {
-	this->setHour(hour);
-	this->setMinute(minute);
-	this->setSecond(second);
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setDate( int day ) {
-	this->setDay(day);
-};
-
-int LN_FAST_CLOCK_SLOT_DATA::getDate() {
-	return this->getDay();
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setSynchronizedRunning( bool sync, bool run ) {
+void LN_FAST_CLOCK_SLOT_DATA::setSynchronizedRunning(bool sync, bool run){
 	this->setSynchronized(sync);
 	this->setRunning(run);
-};
+}
 
-void LN_FAST_CLOCK_SLOT_DATA::setSynchronizedRunning( bool sync, bool run, byte rate ) {
+void LN_FAST_CLOCK_SLOT_DATA::setSynchronizedRunning(bool sync, bool run, byte rate){
 	this->setSynchronized(sync);
 	this->setRunning(run);
 	this->setRate(rate);
-};
+}
 
-void LN_FAST_CLOCK_SLOT_DATA::setSynchronizedRunning( bool sync, bool run, byte rate, int day, byte hour, byte minute, byte second ) {
+void LN_FAST_CLOCK_SLOT_DATA::setSynchronizedRunning(bool sync, bool run, byte rate, int day, byte hour, byte minute, byte slot){
 	this->setSynchronized(sync);
 	this->setRunning(run);
 	this->setRate(rate);
 	this->setDay(day);
 	this->setHour(hour);
 	this->setMinute(minute);
-	this->setSecond(second);
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setSynchronizedRunning( bool sync, bool run, byte rate, int day, byte hour, byte minute, byte second, byte slot ) {
-	this->setSynchronized(sync);
-	this->setRunning(run);
-	this->setRate(rate);
-	this->setDay(day);
-	this->setHour(hour);
-	this->setMinute(minute);
-	this->setSecond(second);
 	this->setSlot(slot);
-};
+}
 
-void LN_FAST_CLOCK_SLOT_DATA::setFastClock( bool isFastClock ) {
-	if (isFastClock) {
-		this->data[2] |= 0x08; // Set the fast clock bit
-	} else {
-		this->data[2] &= 0xF7; // Clear the fast clock bit
-	}
+byte LN_FAST_CLOCK_SLOT_DATA::getDeviceId1(){
+	return this->data[11];
+}
+
+byte LN_FAST_CLOCK_SLOT_DATA::getDeviceId2(){
+	return this->data[12];
+}
+
+void LN_FAST_CLOCK_SLOT_DATA::setDeviceId1(byte id1){
+	this->data[11] = id1;
 	this->setCheckSum();
-};
+}
 
-bool LN_FAST_CLOCK_SLOT_DATA::isFastClock() {
-	return (this->data[2] & 0x08) != 0; // Check the fast clock bit
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setFastClock( bool isFastClock, byte rate ) {
-	this->setFastClock(isFastClock);
-	this->setRate(rate);
-};
-void LN_FAST_CLOCK_SLOT_DATA::setFastClock( bool isFastClock, byte rate, int day, byte hour, byte minute, byte second ) {
-	this->setFastClock(isFastClock);
-	this->setRate(rate);
-	this->setDay(day);
-	this->setHour(hour);
-	this->setMinute(minute);
-	this->setSecond(second);
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setFastClock( bool isFastClock, byte rate, int day, byte hour, byte minute, byte second, byte slot ) {
-	this->setFastClock(isFastClock);
-	this->setRate(rate);
-	this->setDay(day);
-	this->setHour(hour);
-	this->setMinute(minute);
-	this->setSecond(second);
-	this->setSlot(slot);
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setFastClockPacket(bool isFastClockPacket) {
-	if (isFastClockPacket) {
-		this->data[2] |= 0x08; // Set the fast clock bit
-	} else {
-		this->data[2] &= 0xF7; // Clear the fast clock bit
-	}
+void LN_FAST_CLOCK_SLOT_DATA::setDeviceId2(byte id2){
+	this->data[12] = id2;
 	this->setCheckSum();
-};
-
-bool LN_FAST_CLOCK_SLOT_DATA::isFastClockPacket() {
-	return (this->data[2] & 0x08) != 0; // Check the fast clock bit
-};
-
-void LN_FAST_CLOCK_SLOT_DATA::setSyconizedRunning( bool isSynchronizedRunning ) {
-	if (isSynchronizedRunning) {
-		this->data[2] |= 0x01; // Set the synchronized bit
-		this->data[2] |= 0x02; // Set the running bit
-	} else {
-		this->data[2] &= 0xFE; // Clear the synchronized bit
-		this->data[2] &= 0xFD; // Clear the running bit
-	}
-	this->setCheckSum();
-};
-
-bool LN_FAST_CLOCK_SLOT_DATA::isSynchronizedRunning() {
-	return (this->data[2] & 0x01) != 0 && (this->data[2] & 0x02) != 0; // Check the synchronized and running bits
-};
+}
 
 LN_FAST_CLOCK_SLOT_DATA::~LN_FAST_CLOCK_SLOT_DATA() {
-	// TODO Auto-generated destructor stub
+	// no-op
 }
 
 } /* namespace LocoNet */
--- ArduinoLibs/libraries/LocoNet/src/ln_fast_clock_slot_data.h
+++ ArduinoLibs/libraries/LocoNet/src/ln_fast_clock_slot_data.h
@@ -17,43 +17,50 @@ namespace LocoNet {
 		LN_FAST_CLOCK_SLOT_DATA( LNPacket &packet );
 		LN_FAST_CLOCK_SLOT_DATA( packet_data &pdata );
 		virtual arduino::String toString();
+
+		// Rate: 0=freeze, 1=normal 1:1, 10=10:1, etc.
 		byte getRate();
 		void setRate( byte rate );
+		// Fractional minute counter (two bytes as present on the wire)
+		byte getFracMinLow();
+		byte getFracMinHigh();
+		void setFracMinLow(byte v);
+		void setFracMinHigh(byte v);
+		// Combined 16-bit fractional minute value helper (msb: high, lsb: low)
+		uint16_t getFracMin();
+		void setFracMin(uint16_t v);
+		// Minute and Hour are stored in packet as (256 - value) modulo range
+		byte getMinute();
+		void setMinute( byte minute );
+		byte getHour();
+		void setHour( byte hour );
+		// Days (number of 24 hour rolls)
 		int getDay();
 		void setDay( int day );
-		byte gethour();
-		void setHour( byte hour );
-		void setMinute( byte minute );
-		byte getMinute();
-		void setSecond( byte second );
-		byte getSecond();
-		void setSynchronized( bool sync );
-		bool isSynchronized();
-		void setRunning( bool run );
-		bool isRunning();
+		// Slot / Track field
 		void setSlot( byte slot );
 		byte getSlot();
-		void setTime( byte hour, byte minute, byte second );
-		void setDate( int day );
-		int getDate();
-		void setFastClock( bool isFastClock );
-		bool isFastClock();
-		void setFastClockPacket(bool isFastClockPacket);
-		bool isFastClockPacket();
-		void setSyconizedRunning( bool isSynchronizedRunning );
-		bool isSynchronizedRunning();
-		void setDayLightSavings( bool isDaylightSavings );
-		void setSynchronizedRunning( bool sync, bool run );
-		void setSynchronizedRunning( bool sync, bool run, byte rate );
-		void setSynchronizedRunning( bool sync, bool run, byte rate, int day, byte hour, byte minute, byte second );
-		void setSynchronizedRunning( bool sync, bool run, byte rate, int day, byte hour, byte minute, byte second, byte slot );
-		void setFastClock( bool isFastClock, byte rate );
-		void setFastClock( bool isFastClock, byte rate, int day, byte hour, byte minute, byte second );
-		void setFastClock( bool isFastClock, byte rate, int day, byte hour, byte minute, byte second, byte slot );
-		/**
-		 * Destructor
-		 */
-		virtual ~LN_FAST_CLOCK_SLOT_DATA();
+		// Clock control byte access and helpers. The raw control byte is provided
+		byte getClockControl();
+		void setClockControl(byte ctrl);
+		// Convenience flags (operate on bits within the clock control byte)		
+		// D6: 1 = This is valid clock information
+		bool isValid();
+		void setValid(bool valid);
+		// Synchronized / Running helpers (interpretation depends on implementation)		bool isSynchronized();
+		void setSynchronized(bool sync);
+		bool isRunning();
+		void setRunning(bool run);
+		// Set both sync and run in one call and optionally rate/slot/day/time
+		void setSynchronizedRunning(bool sync, bool run);
+		void setSynchronizedRunning(bool sync, bool run, byte rate);
+		void setSynchronizedRunning(bool sync, bool run, byte rate, int day, byte hour, byte minute, byte slot);
+		// Device ID bytes (who last set the clock)		
+		byte getDeviceId1();
+		byte getDeviceId2();
+		void setDeviceId1(byte id1);
+		void setDeviceId2(byte id2);
+		virtual ~LN_FAST_CLOCK_SLOT_DATA();
 	};
 
 } /* namespace LocoNet */
